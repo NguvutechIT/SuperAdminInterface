75205cafe401e217a7d1daf74e2e3c28dc5ad0f6
unknown <chizaanicet@gmail.com>
2023-08-14 10:08:46 +0000

    add to my repository

--- Option/js/app.js
+++ Option/js/app.js
@@ -2,5 +2,5 @@ const icon = document.querySelector(".icon")
     const barMenu = document.querySelector(".nav-menu")
 
     icon.addEventListener('click',()=>{
-    barMenu.classList.toggle('mobiles-menu')
+    barMenu.classList.toggle('mobile-menu')
     })
